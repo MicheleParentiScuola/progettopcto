47488e2ea0c84031182737bab8b28aa3e938a8ca
MicheleParentiScuola <150003073+MicheleParentiScuola@users.noreply.github.com>
2025-03-04 15:25:44 +0100

    Commit 23
    Commit 23: refresh alla pressione del pulsante logout nella pagina home e traduzione in inglese della pagina privacy

--- c#/progetto/obj/Debug/net9.0/progetto.AssemblyInfo.cs
+++ c#/progetto/obj/Debug/net9.0/progetto.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("progetto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ca8ef7937c62009c8fa24accdbf252749c530ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45600112cfc3ccc22b8cc56d1123fe5963fc3bef")]
 [assembly: System.Reflection.AssemblyProductAttribute("progetto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("progetto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
